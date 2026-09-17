022fd717b40a8c817aeb30e7dd541ec275146ba5
Malik05255 <mmbbrr99@gmail.com>
2026-09-17 18:58:52 +0300

    feat: stream cloud media to local archive

--- app/lib/api.dart
+++ app/lib/api.dart
@@ -139,6 +139,35 @@ class ApiClient {
     _ensureOk(response);
   }
 
+  Future<File> downloadMedia(String jobId, Directory directory) async {
+    await init();
+    final request = http.Request('GET', Uri.parse('$baseUrl/v1/jobs/$jobId/media'));
+    request.headers['Authorization'] = 'Bearer $_token';
+    final response = await http.Client().send(request);
+    if (response.statusCode < 200 || response.statusCode >= 300) {
+      final body = await response.stream.bytesToString();
+      throw Exception('HTTP ${response.statusCode}: $body');
+    }
+
+    var extension = '';
+    final disposition = response.headers['content-disposition'] ?? '';
+    final filenameMatch = RegExp(r'filename="?([^";]+)').firstMatch(disposition);
+    final filename = filenameMatch?.group(1) ?? '';
+    final dot = filename.lastIndexOf('.');
+    if (dot >= 0 && filename.length - dot <= 12) extension = filename.substring(dot).toLowerCase();
+
+    final target = File('${directory.path}${Platform.pathSeparator}source$extension');
+    final sink = target.openWrite();
+    try {
+      await response.stream.pipe(sink);
+    } catch (_) {
+      await sink.close();
+      if (await target.exists()) await target.delete();
+      rethrow;
+    }
+    return target;
+  }
+
   Future<void> deleteJob(String jobId) async {
     final response = await http.delete(Uri.parse('$baseUrl/v1/jobs/$jobId'), headers: await _headers());
     _ensureOk(response);
